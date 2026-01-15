2bad026d2e8c94505d91012a9f25f86cedea7348
Tine <tine@tjo.space>
2026-01-15 23:03:51 +0100

    chore: dns changes

--- dns/main.tf
+++ dns/main.tf
@@ -32,6 +32,32 @@ resource "desec_rrset" "ingress" {
   records = each.value.type == "A" ? data.dns_a_record_set.ingress.addrs : data.dns_aaaa_record_set.ingress.addrs
   ttl     = 3600
 }
+resource "desec_rrset" "https" {
+  for_each = toset([
+    "",
+    "cloud",
+    "code",
+    "collabora",
+    "chat",
+    "turn.chat",
+    "mas.chat",
+    "matrix.chat",
+    "webhook.chat",
+    "*.media",
+    "media",
+    "paperless",
+    "rss",
+    "search",
+    "send",
+    "vault",
+  ])
+
+  domain  = "tjo.space"
+  subname = each.value
+  type    = "HTTPS"
+  records = ["0 any.ingress.tjo.cloud."]
+  ttl     = 3600
+}
 
 locals {
   records = [
@@ -58,14 +84,14 @@ locals {
     { type = "TXT", subdomain = "_dmarc", records = ["v=DMARC1; p=reject; rua=mailto:postmaster@tjo.space; ruf=mailto:postmaster@tjo.space"] },
     { type = "TXT", subdomain = "_smtp._tls", records = ["v=TLSRPTv1; rua=mailto:postmaster@tjo.space"] },
     { type = "TLSA", subdomain = "_25._tcp.mail", records = [
-      "3 0 1 2d210b2bd7e921e1a7fd11a80b527fe84dff45e186535dc0d1fc35e0b84b67cc",
-      "3 0 2 d4db783180cf8ecb5cb6e2d72d6f774e293d92c2ac3e92b7068c17c5148f8a7b6eb06bce0f5b861f19ad526e60d37174b5212ed05bb88566752eba83011d3c88",
-      "3 1 1 ec5cd6782de55a70ec41bdcebefcac8ee6febd7777f65931fc5679ade6c2b04b",
-      "3 1 2 4f8a71b84006c5f7d4d68793aa4bece277e90515ca44eea3ca665f629e8c8ec29df4d58d1bba5101e4746a1061771843a69c16b0b2155a8614a483e59832a438",
-      "2 0 1 aeb1fd7410e83bc96f5da3c6a7c2c1bb836d1fa5cb86e708515890e428a8770b",
-      "2 0 2 e18f3d6ccbc578f025c3c7c29ed7bffe1b8eef5b1f839c17298dcf218303d2a63e305f6c1f489691774a18bad836035e5af2de1fc42a3a26cfe9e530f92e3855",
-      "2 1 1 cbbc559b44d524d6a132bdac672744da3407f12aae5d5f722c5f6c7913871c75",
-      "2 1 2 7d779dd26d37ca5a72fd05f1b815a06078c8e09777697c651fbe012c8d2894e048fcfe24160ee1562602240b6bef44e00f2b7340c84546d6110842bbdeb484a7",
+      "3 0 1 d40473bb22eeeec619ffa7972ae7ee1d4e05b2670344c69014377b0b279201c3",
+      "3 0 2 423a7893922342406d706fd8a8f6a378f2ca716b0e45c924a2918b557d7e03a015ce507efa9cf588a6a236bbd93cf522cfa26e78867964ca7b36c7241f5528cc",
+      "3 1 1 c04cfe7604955bb2c6c57f900c5ca4655af6d1eec5d0a8c4fae4f7256cdfd020",
+      "3 1 2 095b866ed351e019a74da79bcd11087a8427f1c91f6548e4ce64bee658997d49ed1a8ae5d4b52115da892546e63a82f4e2a034aeae3d4af6c2b207a1a2048a0e",
+      "2 0 1 83624fd338c8d9b023c18a67cb7a9c0519da43d11775b4c6cbdad45c3d997c52",
+      "2 0 2 3565cd99fb0bccf03019e4d2276ca5d7c913a3af1ad58a95a8cad181699364f22fb6dc6cc01e071847db3336ae9a122b968d31c5be9a4443e145daba2a1782c6",
+      "2 1 1 885bf0572252c6741dc9a52f5044487fef2a93b811cdedfad7624cc283b7cdd5",
+      "2 1 2 89d8f1d26d16e94600405c8585e40ad1ecde0023cd447e8b39fd90bc8b482c7bd68d963156e5037023b144ec4caa03af8213296f3a498f69dee691a95a92d722",
     ] },
     ## SYSTEM
     { type = "A", subdomain = "batuu.system", records = ["100.65.175.106"] },
